59b13e521b4718ec6bec1db0dc6971386e908e13
Katie Lonsdale <katie952@gmail.com>
2023-02-24 09:27:38 -0700

    test: add write test

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -20,4 +20,15 @@ RSpec.describe Book do
       expect(@charlotte_bronte.books).to eq([])
     end
   end
+
+  describe '#write' do
+    it 'create an instance of book' do
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      
+      expect(jane_eyre).to be_a Book
+      expect(jane_eyre.author).to eq('Charlotte Bronte')
+      expect(jane_eyre.title).to eq('Jane Eyre')
+      expect(jane_eyre.publication_year).to eq('1847')
+    end
+  end
 end
