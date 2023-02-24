c9385b8cc658adb8428d7e7a1c0bb20b7a5ad793
Katie Lonsdale <katie952@gmail.com>
2023-02-24 10:04:59 -0700

    test: add publication time frame tests

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -39,5 +39,18 @@ RSpec.describe Library do
 
       expect(@library.books).to eq([jane_eyre, villette])
     end
+    # try with multiple authors and books
+  end
+
+  describe '#publication_time_frame_for' do
+    it 'returns hash with start and end year of given authors books' do
+      jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      villette = @charlotte_bronte.write("Villette", "1853")
+      @library.add_author(@charlotte_bronte)
+
+      expect(@library.publication_time_frame_for(@charlotte_bronte)).to be_a Hash
+      expect(@library.publication_time_frame_for(@charlotte_bronte)).
+      to eq({:start => '1847', :end => '1853'})
+    end
   end
 end
