d0adad985a28ae30e99369562128227c01355f60
Katie Lonsdale <katie952@gmail.com>
2023-02-24 09:18:20 -0700

    feat: add initialize and helper method to extract year from date

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,13 @@
 class Book
-  
+  attr_reader :title, :author, :publication_year
+
+  def initialize(info)
+    @author = "#{info[:author_first_name]} #{info[:author_last_name]}"
+    @title = info[:title]
+    @publication_year = extract_year(info[:publication_date])
+  end
+
+  def extract_year(date)
+    @publication_date = date.split[2]
+  end
 end
