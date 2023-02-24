b535a9e4704f4ca3bc87e4ece22f31e800ebe680
Katie Lonsdale <katie952@gmail.com>
2023-02-24 09:29:10 -0700

    refactor: initialize method to keep first and last name

--- lib/author.rb
+++ lib/author.rb
@@ -2,7 +2,13 @@ class Author
   attr_reader :name, :books
 
   def initialize(author_name)
-    @name = "#{author_name[:first_name]} #{author_name[:last_name]}"
+    @first_name = author_name[:first_name]
+    @last_name = author_name[:last_name]
+    @name = "#{@first_name} #{@last_name}"
     @books = []
+  end
+
+  def write
+
   end
 end
