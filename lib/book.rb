d71719358cc38c26d9987c08701ff069b954cbeb
Katie Lonsdale <katie952@gmail.com>
2023-02-24 10:43:38 -0700

    feat: add check outs instance variable

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,12 @@
 class Book
   attr_reader :title, :author, :publication_year
+  attr_accessor :check_outs
 
   def initialize(info)
     @author = "#{info[:author_first_name]} #{info[:author_last_name]}"
     @title = info[:title]
     @publication_year = extract_year(info[:publication_date])
+    @check_outs = 0
   end
 
   def extract_year(date)
